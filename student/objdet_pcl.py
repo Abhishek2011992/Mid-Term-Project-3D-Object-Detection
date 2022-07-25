b44cbd67aa8869ef5536b203382bf81405fd7b31
Abhishek U H <abhishek2011992@gmail.com>
2022-07-25 12:50:09 +0530

    final

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -171,11 +171,11 @@ def bev_from_pcl(lidar_pcl, configs):
     img_intensity = intensity_map * 256
     img_intensity = img_intensity.astype(np.uint8)
 
-    while (1):
-        cv2.imshow('img_intensity', img_intensity)
-        if cv2.waitKey(10) & 0xFF == 27:
-            break
-    cv2.destroyAllWindows()
+    #while (1):
+    #    cv2.imshow('img_intensity', img_intensity)
+    #    if cv2.waitKey(10) & 0xFF == 27:
+    #        break
+    #cv2.destroyAllWindows()
 
     #######
     ####### ID_S2_EX2 END ####### 
@@ -202,11 +202,11 @@ def bev_from_pcl(lidar_pcl, configs):
     ## step 3 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
     img_height = height_map * 256
     img_height = img_height.astype(np.uint8)
-    while (1):
-        cv2.imshow('img_height', img_height)
-        if cv2.waitKey(10) & 0xFF == 27:
-            break
-    cv2.destroyAllWindows()
+    #while (1):
+    #    cv2.imshow('img_height', img_height)
+    #    if cv2.waitKey(10) & 0xFF == 27:
+    #        break
+    #cv2.destroyAllWindows()
     #######
     ####### ID_S2_EX3 END #######
 
